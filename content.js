82ee69fe85dc857c362cdf79890eef0b59f2811e
chonlam <1685020647@qq.com>
2024-03-06 15:15:06 +0800

    chore: add web component lib

--- content.js
+++ content.js
@@ -1,3 +1,42 @@
+// render ollama tabbar
+function renderOllamaTab() {
+    const tab = document.createElement("ollama-bar")
+    document.body.appendChild(tab)
+
+    // no highlight case
+    document.addEventListener("selectionchange", () => {
+        if (getSelectedText().length === 0) {
+          tab.setAttribute()
+        }
+    })
+}
+
+function getSelectedText() {
+    window.getSelection().toString()
+}
+
+document.addEventListener("click", () => {
+  if (getSelectedText().length > 0) {
+    showOllamaTab(getTabPosition());
+  }
+});
+
+
+function getMarkerPosition() {
+  const rangeBounds = window
+    .getSelection()
+    .getRangeAt(0)
+    .getBoundingClientRect();
+  return {
+    // Substract width of marker button -> 40px / 2 = 20
+    left: rangeBounds.left + rangeBounds.width / 2 - 20,
+    top: rangeBounds.top - 30,
+    display: "flex",
+  };
+}
+
+renderOllamaTab()
+
 document.addEventListener('mousedown', e => {
     if (e.button !== 2) {
         return false
@@ -11,3 +50,4 @@ document.addEventListener('mousedown', e => {
         return true
     })
 })
+
---
+++ tab.js
@@ -0,0 +1,1 @@
+// ollama popup tab
